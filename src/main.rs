6c0019401b22868674d6794ec3226fafe886d741
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-20 20:53:24 +0700

    list all possible action

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+use std::env;
+
+enum Action {
+    Add(String, u32),
+    List,
+    Summary(Option<u8>),
+    Delete(u32),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let mut actions: HashMap<&str, String> = HashMap::new();
+
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "add" | "list" | "summary" | "delete" | "export" => {}
+            "--description" => {}
+            "--amount" => {}
+            "--id" => {}
+            "--month" => {}
+            "--category" => {}
+            _ => {}
+        }
+    }
+    println!("{:?}", actions);
 }
